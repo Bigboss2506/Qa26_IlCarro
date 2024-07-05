11d8e01f2949ff4a1c970a1b2e96453e43e465cc
Bigboss2506 <belyak2506@gmail.com>
2024-07-05 11:35:18 +0300

    login success

--- Qa26_IlCarro/src/test/java/tests/LoginTests.java
+++ Qa26_IlCarro/src/test/java/tests/LoginTests.java
@@ -1,12 +1,55 @@
 package tests;
 
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class LoginTests extends TestBase {
 
+    @BeforeMethod
+    public void preCondition(){
+        if(app.getHelperUser().isLogged()){
+            app.getHelperUser().logout();
+        }
+    }
+
+    @AfterMethod
+    public void postCondition(){
+        app.getHelperUser().clickOkButton();
+
+    }
+
 
     @Test
-    public void test(){
+    public void loginSuccess(){
+        //margo@gmail.com
+        //Mmar123456$
+        app.getHelperUser().openLoginForm();
+        app.getHelperUser().fillLoginForm("margo@gmail.com", "Mmar123456$");
+        app.getHelperUser().submitLogin();
+
+        //Assert ---> if element with text "Logged in success" is present
+        Assert.assertEquals(app.getHelperUser().getMessage(), "Logged in success");
+
+        //app.getHelperUser().clickOkButton();
+
+
+    }
+
+    @Test
+    public void loginSuccessModel(){
+        //margo@gmail.com
+        //Mmar123456$
+        app.getHelperUser().openLoginForm();
+        app.getHelperUser().fillLoginForm("margo@gmail.com", "Mmar123456$");
+        app.getHelperUser().submitLogin();
+
+        //Assert ---> if element with text "Logged in success" is present
+        Assert.assertEquals(app.getHelperUser().getMessage(), "Logged in success");
+
+        //app.getHelperUser().clickOkButton();
+
 
     }
 }
